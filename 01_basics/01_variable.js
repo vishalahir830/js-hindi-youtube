30cb2d999af5f027158d11ee696c17c15fa69ab0
vishalahir830 <vishalchandera74@gmail.com>
2024-06-27 07:17:46 +0000

    datatype

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 9182828
+let accountEmail ="vishalchandera74@gmail.com"
+var accountPassword ="12121222"
+accountCity ="ahmedabad"
+let accountState;
+
+
+console.log(accountId);
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all code as newer variation
+
+
+let age = 18
+
+let isLoggedIn = false
+
+let state;
+
+
+
+
+//object
+
+console.log(typeof "vishal");
