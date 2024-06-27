68aebfef2446d037bfcb3bdc2756286548439f5d
vishalahir830 <vishalchandera74@gmail.com>
2024-06-27 09:18:38 +0000

    initial commit

--- 01_basics/03_conversinOpretion.js
+++ 01_basics/03_conversinOpretion.js
@@ -7,3 +7,13 @@ console.log(typeof(score));
 
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
+
+
+// **********************Opretion********************
+
+
+
+let value = 3
+let negValue = -value
+
+console.log(negValue);
