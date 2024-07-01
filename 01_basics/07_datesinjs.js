7ea12e4a3ce0a66d28516483dbf625a60e342b48
vishalahir830 <vishalchandera74@gmail.com>
2024-07-01 12:14:56 +0000

    date

---
+++ 01_basics/07_datesinjs.js
@@ -0,0 +1,8 @@
+let myDate = new Date()
+
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toUTCString());
+console.log(myDate.getTimezoneOffset());
+console.log(typeof myDate);
