5008144dc8c86b88669cc6a201b7e05249f72182
vishalahir830 <vishalchandera74@gmail.com>
2024-07-02 06:04:46 +0000

    array1

---
+++ 02_basics/01_arry.js
@@ -0,0 +1,48 @@
+//Arry
+
+
+const myArr = [0,1,2,3,4,5]
+
+const myHeros = ["shaktiman","naagarjun"]
+
+const myArr2 = new Array(1,2,3,4,5)
+
+// console.log(myArr[1]);
+// console.log(myArr[2]);
+
+
+// Array method
+
+// myArr.push(6)
+// myArr.pop()
+
+// myArr.unshift(9);
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+
+// console.log(myArr.lastIndexOf(19));
+
+// const newArr = myArr.join()
+
+
+// console.log(myArr);
+// console.log(typeof newArr);
+
+// slice, splice
+
+console.log("A ", myArr);
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+
+
+
+
+
+
+
+
+
