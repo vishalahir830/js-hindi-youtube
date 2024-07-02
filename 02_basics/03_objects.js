e09f730502d895ef6312852ee63e80bf5c616a27
vishalahir830 <vishalchandera74@gmail.com>
2024-07-02 10:18:01 +0000

    object

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,28 @@
+// singaleton
+
+//objectliteral
+
+const mySym = Symbol("key1");
+
+const JsUser = {
+    name: "vishal",
+    "full name" : "vishalChandera",
+    [mySym]: "key1",
+    age: 22,
+    location: "ahmedabad",
+    email: "vishal@google.com",
+    isLoggedIn: false,
+    lastLoggInDays: ["monday","saturday"]
+    
+};
+
+console.log( JsUser.email);
+console.log( JsUser["email"]);
+console.log(JsUser["full name"]);
+console.log(JsUser[mySym]);
+
+JsUser.email = "vishalll@google.com"
+Object.freeze(JsUser)
+
+
+
