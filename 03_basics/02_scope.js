25fc57e1476e2656ea42a16287569470150c3c98
vishal <vishalchandera74@gmail.com>
2024-07-04 15:14:42 +0530

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,35 @@
+let a = 20
+var b = 40
+const c = 50
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one () {
+    const username = "vishal"
+
+    function two (){
+        const website = "youtube"
+        console.log(username);
+    }
+
+    //   console.log(website);
+
+      two()
+}
+
+one()
+
+if (true) {
+    const username = "vishal"
+    if (username === "vishal") {
+        const website = "youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+
+}
+
+// console.log(username);
